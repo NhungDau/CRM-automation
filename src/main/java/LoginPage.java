0d2f99a51afd51a28ae4c5d049aa6925cad31ee5
NhungDau <nhungdaulephuong@gmail.com>
2024-12-02 21:12:31 +0700

    Init project

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -2,5 +2,6 @@ import org.openqa.selenium.By;
 
 public class LoginPage {
     By emailTextBoxLocator = By.id("campaigntypeform:email");
+    By passwordTextBox =By.id("campaigntypeform:email") ;
 
 }
