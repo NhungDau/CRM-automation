0e87085f290a7979f72dff62c19daf949e7ad15d
hanhtran215 <hanhtran21599@gmail.com>
2024-12-05 23:06:43 +0700

    Update 05.12

--- src/main/java/model/ProductOrderInformation.java
+++ src/main/java/model/ProductOrderInformation.java
@@ -2,4 +2,44 @@ package model;
 
 public class ProductOrderInformation {
 
+    private String productName;
+    private Integer productQuantity;
+    private Double productPrice;
+
+    public ProductOrderInformation(String productName, Double productPrice, Integer productQuantity) {
+        this.productName = productName;
+        this.productQuantity = productQuantity;
+        this.productPrice = productPrice;
+    }
+
+    public ProductOrderInformation() {
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(Integer productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public ProductOrderInformation productOrderInformation(String productName, double price, int quantity){
+        return new ProductOrderInformation(this.productName,productPrice,productQuantity);
+    }
 }
--- src/main/java/page/CreateOrderPage.java
+++ src/main/java/page/CreateOrderPage.java
@@ -1,20 +1,31 @@
 package page;
 
+import model.ProductOrderInformation;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Random;
 
-public class CreateOrderPage {
+public class CreateOrderPage extends ProductOrderInformation {
 
     By getListProductByNameLocator = By.xpath("//tr[td/input[@type='checkbox']]/td[2]");
+    By getListCheckboxLocator = By.xpath("//tr/td/input[@type='checkbox']");
+    By getListQuantityTextBoxLocator = By.xpath("//tr[td/input[@type='checkbox']]/td[4]/input");
+    By getListProductPriceLocator = By.xpath("//tr[td/input[@type='checkbox']]/td[3]");
     By paymentDateTextboxLocator = By.xpath("//input[@id='j_idt74:pd']");
 
+
     Random random = new Random();
+    LocalDate today = LocalDate.now();
+    // date format yyy-mm-dd
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    String paymentdate = today.format(formatter);
 
-
+    int quantity = random.nextInt(20)+1;
 
     WebDriver driver;
 
@@ -22,11 +33,44 @@ public class CreateOrderPage {
         this.driver = driver;
     }
 
+    //enter paymentdate
+    public void enterPaymentDate(String paymentDate){
+        driver.findElement(paymentDateTextboxLocator).sendKeys(paymentDate);
+    }
+    //click checkbox
+    public void clickCheckboxByIndex(int index){
+        List<WebElement> list = driver.findElements(getListCheckboxLocator);
+        list.get(index).click();
+    }
+    //enter product quantity
+    public void enterQuantityByIndex(int index){
+        List<WebElement> list = driver.findElements(getListQuantityTextBoxLocator);
+        list.get(index).sendKeys(String.valueOf(quantity));
+    }
+    //get product price
+    public double getProductPriceByIndex(int index){
+        List<WebElement> list = driver.findElements(getListProductPriceLocator);
+        return Double.parseDouble(list.get(index).getText());
+    }
+
     public void addOrderByIndex(int a){
         List<WebElement> list = driver.findElements(getListProductByNameLocator);
-        int index = random.nextInt(20)+1;
-        int quantity = random.nextInt(list.size()+1);
-        list.get(list.size()-1).click();
+
+        int index = random.nextInt(list.size()+1);
+
+        //enter payment date
+        enterPaymentDate(paymentdate);
+        //check checkbox
+        clickCheckboxByIndex(index);
+        //enter quantity
+        enterQuantityByIndex(index);
+        //get product name
+        String productName = list.get(index).getText();
+        //get product price
+        double price = getProductPriceByIndex(index);
+        productOrderInformation(productName,price,quantity);
+
+
 
     }
 
