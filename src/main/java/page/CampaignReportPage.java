4cc8c9297eb4297403326ca107b0e17da29838ca
hanhtran215 <hanhtran21599@gmail.com>
2024-12-18 20:54:58 +0700

    Update TC05 + TC07

--- src/main/java/page/CampaignReportPage.java
+++ src/main/java/page/CampaignReportPage.java
@@ -3,7 +3,10 @@ package page;
 import model.Campaign;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.Random;
 
 public class CampaignReportPage {
@@ -12,7 +15,7 @@ public class CampaignReportPage {
     By statusLabelLocator = By.xpath("//a[@class='ui-link ui-widget']/../following-sibling::td[2]");
     By startDateLocator = By.xpath("//a[@class='ui-link ui-widget']/../following-sibling::td[3]");
     By endDateLocator = By.xpath("//a[@class='ui-link ui-widget']/../following-sibling::td[4]");
-    By searchByCampaignNameTextboxLocator = By.xpath("//span[text()='Campaign Name']//..//input[@class='ui-column-filter ui-inputfield ui-inputtext ui-widget ui-state-default ui-corner-all']");
+    By searchByCampaignNameTextboxLocator = By.xpath("//span[text()='Campaign Name']//..//input");
     By searchByCampaignTypeTextboxLocator = By.xpath("//span[text()='Type']//..//input[@class='ui-column-filter ui-inputfield ui-inputtext ui-widget ui-state-default ui-corner-all']");
     By searchByStatusTextboxLocator = By.xpath("//span[text()='Status']//..//input[@class='ui-column-filter ui-inputfield ui-inputtext ui-widget ui-state-default ui-corner-all']");
     By searchByStartDateTextboxLocator = By.xpath("//span[text()='Start Date']//..//input[@class='ui-column-filter ui-inputfield ui-inputtext ui-widget ui-state-default ui-corner-all']");
@@ -20,15 +23,20 @@ public class CampaignReportPage {
     Random random = new Random();
 
     WebDriver driver;
+    WebDriverWait wait;
+
+
 
     public CampaignReportPage(WebDriver driver) {
         this.driver = driver;
+        new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     //enter campaign name to search box
     public void searchByCampaignName(String campaignName){
 //        driver.findElement(searchByCampaignNameTextboxLocator).click();
         driver.findElement(searchByCampaignNameTextboxLocator).sendKeys(campaignName);
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(campaignNameLabelLocator, campaignName));
     }
 
     //enter campaign type to search box
